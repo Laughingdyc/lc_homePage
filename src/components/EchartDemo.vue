09fb115fadf5d0ad159cc91e5c2ed153a082489f
My Name Is DuYuchen <duyuchen6324333@qq.com>
2022-04-13 00:31:06 +0800

    feat: 首页

---
+++ src/components/EchartDemo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div id="myChart" :style="{width: '100wv', height: '500px'}"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import * as echarts from 'echarts'
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    msg: String
+  }
+})
+export default class EchartDemo extends Vue {
+  mounted (): void {
+    let _doc: HTMLElement = document.getElementById("myChart") as HTMLElement;
+    let myChart = echarts.init(_doc);
+    // 绘制图表
+    myChart.setOption({
+      title: { text: "总用户量" },
+      tooltip: {},
+      xAxis: {
+        data: ["12-3", "12-4", "12-5", "12-6", "12-7", "12-8"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "用户量",
+          type: "line",
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    });
+    window.onresize = function () {//自适应大小
+      myChart.resize();
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <EchartDemo msg="Welcome to LightCommunity!" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import EchartDemo from '@/components/EchartDemo.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    EchartDemo
   },
 })
 export default class Home extends Vue {}
