1ff5618b9c6b10b149d119fabc4f2d551dbe8000
My Name Is DuYuchen <duyuchen6324333@qq.com>
2022-04-13 02:50:07 +0800

    feat: 优化一下表格数据

--- src/components/EchartDemo.vue
+++ src/components/EchartDemo.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script lang="ts">
+import dayjs from "dayjs";
 import * as echarts from 'echarts'
 import { Options, Vue } from 'vue-class-component';
 
@@ -18,19 +19,21 @@ export default class EchartDemo extends Vue {
   mounted (): void {
     let _doc: HTMLElement = document.getElementById("myChart") as HTMLElement;
     let myChart = echarts.init(_doc);
+    const dateData = this.formatDate()
+    const viewData = this.formatViews()
     // 绘制图表
     myChart.setOption({
       title: { text: "总用户量" },
       tooltip: {},
       xAxis: {
-        data: ["12-3", "12-4", "12-5", "12-6", "12-7", "12-8"],
+        data: dateData,
       },
       yAxis: {},
       series: [
         {
           name: "用户量",
           type: "line",
-          data: [5, 20, 36, 10, 10, 20],
+          data: viewData,
         },
       ],
     });
@@ -38,9 +41,26 @@ export default class EchartDemo extends Vue {
       myChart.resize();
     };
   }
-  // public name() {
-    
-  // }
+  public formatDate():string[] {
+    const resultDate = []
+    const currentDate = new Date().getTime()
+    for (let beforeDays = 0; beforeDays < 30; beforeDays++) {
+      const beforeDateTimeStamp = currentDate - ( 1000 * 60 * 60 * 24 ) * beforeDays
+      resultDate.push(
+        dayjs(beforeDateTimeStamp).format('YYYY-MM-DD')
+      )
+    }
+    return resultDate.reverse();
+  }
+  public formatViews():number[] {
+    const resultViews = []
+    for (let beforeDays = 0; beforeDays < 30; beforeDays++) {
+      resultViews.push(
+        Math.round(Math.random()*30+20)
+      )
+    }
+    return resultViews.reverse();
+  }
 }
 </script>
 
